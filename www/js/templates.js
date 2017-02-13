a2a6c13d91d3ae5c06659815e3c401a976a7c5d5
RamCrodd <ramcrodd@gmail.com>
2017-02-13 19:04:35 +0100

    Pulizia generale, Documento Design v2, Documento Tecnico, Video App

--- www/js/templates.js
+++ www/js/templates.js
@@ -10,7 +10,6 @@
 //  - value: the path of the html file containing the HTML fragment of the template
 define({
 	structure : "templates/structure.html",
-	myview : "templates/myview.html",
 	/**
 	 * elementi
 	 */
@@ -19,7 +18,6 @@ define({
 	carousel : "templates/carousel.html",
 	carouselImmagine : "templates/carouselImmagine.html",
 	errore : "templates/errore.html",
-	menuLaterale : "templates/menuLaterale.html",
 	preloaderCircolare : "templates/preloaderCircolare.html",
 	prodottoCard : "templates/prodottoCard.html",
 	prodottoCarrello : "templates/prodottoCarrello.html",
